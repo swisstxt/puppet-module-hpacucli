9d9edd360b4f69d8c74703234bc19ddda0af3e08
Simon Josi <simon.josi@swisstxt.ch>
2013-03-19 13:21:40 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+#
+# hpacucli module
+#
+# Copyright 2012, SWISS TXT
+# Simon Josi  me (at) yokto (dot) net
+#
+# This program is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License version 3 as published by
+# the Free Software Foundation.
+#
+
+class hpacucli {
+  package{'hpacucli':
+    ensure => present,
+  }
+}
